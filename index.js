2113ef05982c86fc33c207f9773d89913334263f
Anupam Khargharia <kharghariaanupam07@gmail.com>
2023-12-03 16:11:41 -0500

    Card transition smooth

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/*****************************************************************************
- * Challenge 2: Review the provided code. The provided code includes:
- * -> Statements that import data from games.js
- * -> A function that deletes all child elements from a parent element in the DOM
-*/
-
 // import the JSON data about the crowd funded games from the games.js file
 import GAMES_DATA from './games.js';
 
@@ -17,21 +11,19 @@ function deleteChildElements(parent) {
     }
 }
 
-/*****************************************************************************
- * Challenge 3: Add data about each game as a card to the games-container
- * Skills used: DOM manipulation, for loops, template literals, functions
-*/
 
 // grab the element with the id games-container
 const gamesContainer = document.getElementById("games-container");
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
+    // Clear existing games
+    while (gamesContainer.firstChild) {
+        gamesContainer.removeChild(gamesContainer.firstChild);
+    }
 
-    // loop over each item in the data
-    for (let i = 0; i < games.length; i++){
-        const game = games[i];
-
+    // Append new games with a delay to allow for CSS transition
+    games.forEach((game, index) => {
         const gameCard = document.createElement('div');
         gameCard.classList.add('game-card');
 
@@ -42,20 +34,19 @@ function addGamesToPage(games) {
             <p><strong>Pledged:</strong> ${game.pledged}</p>
             <p><strong>Backers:</strong> ${game.backers}</p>
         `;
+
         gamesContainer.appendChild(gameCard);
-    }
+
+        // Set a timeout to change opacity for the fade-in effect
+        setTimeout(() => {
+            gameCard.style.opacity = 1;
+            gameCard.style.transform = 'translateY(0)';
+        }, 100 * index); // Delay each card slightly more than the previous one
+    });
 }
+// call the function we just defined using the correct variable
 addGamesToPage(GAMES_JSON);
 
-// call the function we just defined using the correct variable
-// later, we'll call this function using a different list of games
-
-
-/*************************************************************************************
- * Challenge 4: Create the summary statistics at the top of the page displaying the
- * total number of contributions, amount donated, and number of games on the site.
- * Skills used: arrow functions, reduce, template literals
-*/
 
 // grab the contributions card element
 const contributionsCard = document.getElementById("num-contributions");
@@ -87,15 +78,8 @@ const totalGames = GAMES_JSON.reduce((acc, game) => {
 
 gamesCard.innerHTML =  `${totalGames.toLocaleString()}`;
 
-
-
-/*************************************************************************************
- * Challenge 5: Add functions to filter the funded and unfunded games
- * total number of contributions, amount donated, and number of games on the site.
- * Skills used: functions, filter
-*/
-
 showAllGames();
+
 // show only games that do not yet have enough funding
 function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
@@ -165,12 +149,8 @@ const allBtn = document.getElementById("all-btn");
 unfundedBtn.addEventListener("click", filterUnfundedOnly);
 fundedBtn.addEventListener("click", filterFundedOnly);
 allBtn.addEventListener("click", showAllGames);
-
 document.getElementById("search-btn").addEventListener("click", searchGames);
-/*************************************************************************************
- * Challenge 6: Add more information at the top of the page about the company.
- * Skills used: template literals, ternary operator
-*/
+
 
 // grab the description container
 const descriptionContainer = document.getElementById("description-container");
@@ -189,10 +169,6 @@ newParagraph.innerHTML = infoString;
 
 descriptionContainer.appendChild(newParagraph); 
 
-/************************************************************************************
- * Challenge 7: Select & display the top 2 games
- * Skills used: spread operator, destructuring, template literals, sort 
- */
 
 const firstGameContainer = document.getElementById("first-game");
 const secondGameContainer = document.getElementById("second-game");
